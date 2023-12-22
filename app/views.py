9b1b4da4ee55427d54f1630ccc256f3cb21114ce
DeveloperUchida <kouei28poiou@icloud.com>
2023-12-22 10:49:09 +0900

    DataUpdate 2023/12/22

--- app/views.py
+++ app/views.py
@@ -1,15 +1,31 @@
-from flask import render_template, request, redirect, url_for
-app = views.app
+from flask import Flask, request, render_template
 
-#検証用DB
+app = Flask(__name__)
+
+# 検証用DB
 users = {'user1': 'password1', 'user2': 'password2'}
 
 @app.route('/')
 def home():
-    return "welcome to the Home Page!!"
+    return "Welcome to the Home Page!!"
 
-@app.rote('/login', method =[ 'GET', 'POST'])
+@app.route('/login', methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
         username = request.form['username']
-        
+        password = request.form['password']
+
+        # ユーザーの存在をチェックする
+        if username in users and users[username] == password:
+            # ログイン成功時メッセージ表示
+            return 'Login successful!!'
+        else:
+            # ログイン失敗時の処理
+            return 'Login failed. Please check your username and password.'
+
+    # GETリクエストが来たときは、login.htmlを表示
+    return render_template('login.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
+    
